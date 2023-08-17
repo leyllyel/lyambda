ee4ac9b79b6957f2a5a4c5447e8e7ffb9c194662
leyl <sad.ley@icloud.com>
2023-08-18 02:28:01 +0300

    new

--- src/Main.java
+++ src/Main.java
@@ -3,14 +3,10 @@ public class Main {
         Calculator calc = Calculator.instance.get();
         int a = calc.plus.apply(1, 2);
         int b = calc.minus.apply(1, 1);
-        //int c = calc.devide.apply(a, b);  произошла арифметическая ошибка "деление на ноль", необходимо ее поймать
-        int c = 0;
-        try {
-            c = calc.devide.apply(a, b);
-            calc.println.accept(c);
-        } catch (ArithmeticException e) {
-            System.out.println("Ошибка: деление на ноль");
-        }
+        //int c = calc.devide.apply(a, b);  произошла арифметическая ошибка "деление на ноль", необходимо сделать проверку
+        int c = calc.devide.apply(a, b);
+        calc.println.accept(c);
+
 
     }
 }
